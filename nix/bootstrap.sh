16fb6d0eefbedf966a0fd4e74a838dedcf8eb782
Jeff Coffler <jeff@taltos.com>
2018-07-31 20:41:26 +0000

    Fix quoting in bootstrap.sh (bash script)
    1. Use "set -eu" for slightly saner bash execution environment
    2. Double quote all the things, to handle paths including whitespace
    3. Use "$(...)" in place of "`...`"

--- nix/bootstrap.sh
+++ nix/bootstrap.sh
@@ -1,4 +1,5 @@
-#! /bin/bash
+#!/bin/bash
+set -eu
 
 # Set up soft links from files to their destination (in home directory)
 
@@ -8,42 +9,42 @@
 # And HP doesn't define $PWD in a sudo environment, so we define our own
 case $0 in
     /*|~*)
-        SCRIPT_INDIRECT="`dirname $0`"
+        SCRIPT_INDIRECT="$(dirname "$0")"
         ;;
     *)
         PWD="`pwd`"
-        SCRIPT_INDIRECT="`dirname $PWD/$0`"
+        SCRIPT_INDIRECT="$(dirname "$PWD/$0")"
         ;;
 esac
 
-BASEDIR="`(cd \"$SCRIPT_INDIRECT\"; pwd -P)`"
+BASEDIR="$(cd "$SCRIPT_INDIRECT" ; pwd -P)"
 
-for i in $BASEDIR/*; do
-    [ ! -d $i ] && continue
+for i in "$BASEDIR"/*; do
+    [ ! -d "$i" ] && continue
 
-    for j in $i/*; do
-        FILEDIR=`dirname $j`
-        FILE=`basename $j`
-        BASEFILE=$HOME/.$FILE
+    for j in "$i"/*; do
+        FILEDIR="$(dirname "$j")"
+        FILE="$(basename "$j")"
+        BASEFILE="$HOME/.$FILE"
  
-        if [ -f $BASEFILE -o -h $BASEFILE ]; then
+        if [ -f "$BASEFILE" -o -h "$BASEFILE" ]; then
             echo "Replacing file: $BASEFILE"
-            rm $BASEFILE
+            rm "$BASEFILE"
         else
             echo "Creating link: $BASEFILE"
         fi
 
-        ln -s $j $BASEFILE
+        ln -s "$j" "$BASEFILE"
     done
 done
 
 # Make a pass deleting stale links, if any
 for i in ~/.*; do
-    [ ! -h $i ] && continue
+    [ ! -h "$i" ] && continue
 
     # We have a link: Is it stale? If so, delete it ...
-    if [ ! -f $i ]; then
+    if [ ! -f "$i" ]; then
         echo "Deleting stale link: $i"
-        rm $i
+        rm "$i"
     fi
 done
